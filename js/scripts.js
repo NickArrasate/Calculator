9854b18feec99a09a172781ef5584d9ae213f0eb
adam craig and nick arrasate <adam@>
2016-03-23 10:07:40 -0700

    Update UI with forms and jquery functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -18,45 +18,80 @@ var divide = function(number1, number2) {
 	return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
+$(document).ready(function() {
 
-var number2 = parseInt(prompt("Enter another number:"));
+ $("form#add").submit(function(event){
+	var number1 = parseInt($("#add1").val());
+	var number2 = parseInt($("#add2").val());
+	var result = add(number1, number2);
+	$("#addoutput").text(result);
 
-var result = divide(number1, number2);
+	event.preventDefault();
 
-alert(result);
+	});
+
+	$("form#subtract").submit(function(event){
+ 	var number1 = parseInt($("#sub1").val());
+ 	var number2 = parseInt($("#sub2").val());
+ 	var result = subtract(number1, number2);
+ 	$("#suboutput").text(result);
+
+ 	event.preventDefault();
+
+ 	});
+
+	$("form#multiply").submit(function(event){
+ 	var number1 = parseInt($("#mult1").val());
+ 	var number2 = parseInt($("#mult2").val());
+ 	var result = multiply(number1, number2);
+ 	$("#multoutput").text(result);
+
+ 	event.preventDefault();
+
+ 	});
+
+	$("form#divide").submit(function(event){
+ 	var number1 = parseInt($("#div1").val());
+ 	var number2 = parseInt($("#div2").val());
+ 	var result = divide(number1, number2);
+ 	$("#divoutput").text(result);
+
+ 	event.preventDefault();
+
+ 	});
+});
 
 /*===BMI Functions
 ====================================*/
 
-var userBmi = function(weight,height) {
-	return (weight * 703) / (height * height);
-};
-
-var weight = parseInt(prompt("How much do you weight?"));
-
-var height = parseInt(prompt("How tall are you?"));
-
-var result = userBmi(height,weight);
-
-alert("Your BMI is " + result.toPrecision(4) + ".");
+// var userBmi = function(weight,height) {
+// 	return (weight * 703) / (height * height);
+// };
+//
+// var weight = parseInt(prompt("How much do you weight?"));
+//
+// var height = parseInt(prompt("How tall are you?"));
+//
+// var result = userBmi(height,weight);
+//
+// // alert("Your BMI is " + result.toPrecision(4) + ".");
 
 /*===Temperature Functions
 ====================================*/
 
-var temp = parseInt(prompt("Enter Fahrenheit to convert to Celcius"));
-
-var fToC = function(temp) {
-	return temp * (9/5) + 32
-};
-
-var cToF = function(temp) {
-	return (temp - 32) * (5/9);
-};
-
-var result = cToF(temp);
-
-alert(result.toPrecision(3));
-
-/*===Unit conversion Functions
-====================================*/
+// var temp = parseInt(prompt("Enter Fahrenheit to convert to Celcius"));
+//
+// var fToC = function(temp) {
+// 	return temp * (9/5) + 32
+// };
+//
+// var cToF = function(temp) {
+// 	return (temp - 32) * (5/9);
+// };
+//
+// var result = cToF(temp);
+//
+// // alert(result.toPrecision(3));
+//
+// /*===Unit conversion Functions
+// ====================================*/
